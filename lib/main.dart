203219d408b5cee32d52eb82b09339e37bb6cb13
darshshah <darshshah1996@gmailcom>
2019-05-05 23:13:05 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+
     return MaterialApp(
+
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('StartUp Name Generator'),
         ),
-        body: Center(
-          //child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+        body: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            //child: Text('Hello World'),
+//          child: TextField(
+//            decoration: InputDecoration(
+//              border: OutlineInputBorder(),
+//            hintText: 'Hello',
+//            labelText: 'Hello'),
+//            textAlign: TextAlign.center,
+//            textInputAction: TextInputAction.continueAction,
+
+
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+
+                decoration: InputDecoration(
+                  hintText: 'Enter Weight in Kgs',
+                  alignLabelWithHint: true,
+                  labelText: 'Enter Weight ',
+                  border: OutlineInputBorder(),
+
+
+                ),
+
+                keyboardType: TextInputType.numberWithOptions(),
+                textAlign: TextAlign.center,
+//                textInputAction: TextInputAction.continueAction,
+                onChanged: (text){
+                  var weight = text as double ;
+                  print(weight);
+                },
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Height in Centimeters',
+                  alignLabelWithHint: true,
+                  labelText: 'Enter Height ',
+                  border : OutlineInputBorder(),
+
+                ),
+
+                keyboardType: TextInputType.numberWithOptions(),
+
+                textAlign: TextAlign.center,
+//                textInputAction: TextInputAction.continueAction,
+              ),
+            )
+
+
+          ],
+
+
+            ),
         ),
-      ),
-    );
+
+        ),
+
+        );
+
   }
 }
